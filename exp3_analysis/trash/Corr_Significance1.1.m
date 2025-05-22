27eea8034c1e5f03d5b5b716b68adc49de92a117
Ryosuke-256 <aaa@aa.com>
2025-05-22 17:22:40 +0900

    0522

--- exp3_analysis/trash/Corr_Significance1.1.m
+++ exp3_analysis/trash/Corr_Significance1.1.m
@@ -8,7 +8,6 @@ dataB = reshape(arrayB,dimsB(1:reshapeNum),[]);
 %}
 
 numBootstrap = bootstrap; 
-illumDim = 1;
 
 % 結果保存用
 correlationDiffs = zeros(numBootstrap, 1);
@@ -18,8 +17,8 @@ corrAB = zeros(numBootstrap,1);
 for i = 1:numBootstrap
     % === 1. データAでの基準相関係数を計算 ===
     % 1.a.b 条件ごとにブートストラップで平均を計算
-    sampleA1 = MeanArray(dataA,illumDim);
-    sampleA2 = MeanArray(dataA,illumDim);
+    sampleA1 = MeanArray(dataA);
+    sampleA2 = MeanArray(dataA);
 
     % 1.c 平均値データ間の相関係数
     corrA(i) = corr(sampleA1(:),sampleA2(:));
@@ -28,7 +27,7 @@ for i = 1:numBootstrap
     if ndims(dataB) == 1
         sampleB = dataB;
     else
-        sampleB = MeanArray(dataB,illumDim);
+        sampleB = MeanArray(dataB);
     end
     % 2.b データAとデータB間の相関係数
     corrAB(i) = corr(sampleA1(:),sampleB(:));
@@ -38,31 +37,13 @@ for i = 1:numBootstrap
 end
 end
 
-% 入力：配列、ベースとする次元(今回は照明条件 = 1次元目)
-function [reducedData] = MeanArray(array,baseDim)
+% 
+function [reducedData] = MeanArray(array)
     dims = ndims(array);
-    %baseDim = 1;
-    shuftleDim = dims;
-    
-    reducedData = zeros(size(array));
-    
-    for i = 1:size(array,baseDim)
-        % 並び替えるためのランダム配列
-        random_idx = randi(size(array, dims), [1, size(array, dims)]);
-    
-        % array側の挿入する配列
-        subs_target = repmat({':'}, 1, dims);
-        subs_target{baseDim} = i;
-        subs_target{shuftleDim} = random_idx;
-    
-        % reducedData側で挿入される配列
-        subs_receive = repmat({':'}, 1, dims);
-        subs_receive{baseDim} = i;
-    
-        % データの挿入
-        reducedData(subs_receive{:}) = array(subs_target{:});
-    end
-    
+    random_idx = randi(size(array, dims), [1, size(array, dims)]);
+    subs = repmat({':'}, 1, dims);
+    subs{dims} = random_idx;
+    reducedData = array(subs{:});
     for d = dims:-1:2
         reducedData = mean(reducedData, d);
     end
