553f17af7e87729e0dc60b0b76dd34228608d0f8
rkdynabook <ryokawa1460@gmail.com>
2025-06-26 01:03:33 +0900

    0625

--- exp3_analysis/Corr_MatShape_Significance.m
+++ exp3_analysis/Corr_MatShape_Significance.m
@@ -1,4 +1,4 @@
-function [corrA,corrAB,correlationDiffs] = Corr_MatShape_Significance(dataA,dataB,bootstrap)
+function [corrA,corrAB,correlationDiffs,threshold] = Corr_MatShape_Significance(dataA,dataB,bootstrap)
 numBootstrap = bootstrap; 
 [numIllumination,MatNum,ShapeNum,~] = size(dataA); % 照明環境の数
 illumDim = 1;
@@ -13,13 +13,17 @@ for mat = 1:MatNum
         for i = 1:numBootstrap
             % === 1. データAでの基準相関係数を計算 ===
             % 1.a.b 条件ごとにブートストラップで平均を計算
-            sampleA1 = mean(dataA(:,:,:, randi(size(dataA, 4), [1, size(dataA, 4)])), 4);
-            sampleA2 = mean(dataA(:,:,:, randi(size(dataA, 4), [1, size(dataA, 4)])), 4);
+            sampleA1 = MeanArray(dataA,illumDim);
+            sampleA2 = MeanArray(dataA,illumDim);
             % 1.c 平均値データ間の相関係数
             corrA(i,mat,shape) = corr(sampleA1(:,mat,shape),sampleA2(:,mat,shape));
 
             % === 2. データAとデータBの相関係数を計算 ===
-            sampleB = mean(dataB(:,:,:, randi(size(dataB, 4), [1, size(dataB, 4)])), 4);
+            if ndims(dataB) == 1
+                sampleB = dataB;
+            else
+                sampleB = MeanArray(dataB,illumDim);
+            end
             % 2.b データAとデータB間の相関係数
             corrAB(i,mat,shape) = corr(sampleA1(:,mat,shape),sampleB(:,mat,shape));
 
@@ -39,5 +43,36 @@ for mat = 1:MatNum
         end
     end
 end
+end
 
+
+% 入力：配列、ベースとする次元(今回は照明) = 1次元目)
+function [reducedData] = MeanArray(array,baseDim)
+    dims = ndims(array);
+    %baseDim = 1;
+    shuftleDim = dims;
+    
+    reducedData = zeros(size(array));
+    
+    for i = 1:size(array,baseDim)
+        % 並び替えるためのランダム配列
+        random_idx = randi(size(array, dims), [1, size(array, dims)]);
+    
+        % array側の挿入する配列
+        subs_target = repmat({':'}, 1, dims);
+        subs_target{baseDim} = i;
+        subs_target{shuftleDim} = random_idx;
+    
+        % reducedData側で挿入される配列
+        subs_receive = repmat({':'}, 1, dims);
+        subs_receive{baseDim} = i;
+    
+        % データの挿入
+        reducedData(subs_receive{:}) = array(subs_target{:});
+    end
+    
+    %今回は照明、材質、形状の情報を残したいので4次元目までを平均
+    for d = dims:-1:4
+        reducedData = mean(reducedData, d);
+    end
 end
--- exp3_analysis/Graph_MatShape_Significance.m
+++ exp3_analysis/Graph_MatShape_Significance.m
@@ -1,4 +1,4 @@
-function [] = Graph_MatShape_Significance(dataA,dataB,corrA,corrAB,threshold,amp,numBootstrap,ShapeNames)
+function [] = Graph_MatShape_Significance(dataA,dataB,corrA,corrAB,threshold,NameA,NameB,numBootstrap,MatNames,ShapeNames,savepath,amp)
 [numIllumination,MatNum,ShapeNum,~] = size(dataA); % 照明環境の数
 
 for mat = 1:MatNum
@@ -16,14 +16,14 @@ for mat = 1:MatNum
         centerValue = mean(corrAB(:,mat,shape));
         upperError = maxValue - centerValue;
         lowerError = centerValue - minValue;
-        
+
         % coef hisgram
         x_axis = shape;
         bar_width = 0.4;
         bar_coef = bar(x_axis,r_value,bar_width, 'FaceColor', 'b', 'DisplayName', 'Coef');
-        
+
         errorbar(x_axis, centerValue, lowerError, upperError, 'o', 'LineWidth', 1.0);
-        
+
         graphtext1 = sprintf('%.2f',r_value);
         text(x_axis-0.1,(abs(r_value)+0.06)*r_value/abs(r_value),graphtext1,'HorizontalAlignment','center','VerticalAlignment','bottom','FontSize',8*amp);
         if threshold(mat,shape) > 0
@@ -40,7 +40,7 @@ for mat = 1:MatNum
         CI95 = fill([x_axis-0.5,x_axis+0.5,x_axis+0.5,x_axis-0.5], ...
             [minorigin,minorigin,maxorigin,maxorigin], ...
             'k', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
-        
+
         aveorigin = mean(corrA(:,mat,shape));
         bar_plot = plot([x_axis-0.5, x_axis+0.5], [aveorigin, aveorigin], 'r--', 'LineWidth', 1.2,'DisplayName', 'Within-subject average');
     end
@@ -48,7 +48,12 @@ for mat = 1:MatNum
     xticklabels(ShapeNames);
     xlabel('Shape types','FontSize',18*amp);
     ylabel('Correlation Coefficient','FontSize',18*amp);
-    %text(x_Limits(1)*0.9,y_Limits(2)*0.95, '* : p < 0.05', 'FontSize', 15*amp);
+    title(sprintf('%s VS %s Correlation Coefficient\n%s',NameA,NameB,string(MatNames(mat))),'FontSize',18*amp);
+    text(x_Limits(1)*0.9,y_Limits(2)*0.95, '* : p < 0.05', 'FontSize', 15*amp);
+    %legend([bar_coef, VR_bar,VR_95CI], {'Row data', 'VR bar','VR 95 CI'}, 'Location', 'best','Orientation','vertical');
+    hold off;
+    plotname = sprintf('%s/%svs%s_Coefbar_%s.jpg',savepath, NameA,NameB,string(MatNames(mat)));
+    saveas(gcf, plotname);
 end
 end
 
